3581f73b80adedbf3ec71087b350ed07c83293b6
Simon Thormeyer <simonthormeyer@gmail.com>
2020-05-08 13:28:12 +0200

    closes #13

---
+++ Frontend/src/components/threeJS/canvas.js
@@ -0,0 +1,8 @@
+import React from 'react';
+//import threeJs
+
+function Canvas() {
+    return <div>Hello from Canvas</div>
+}
+
+export default Canvas;
---
+++ Frontend/src/containers/App.js
@@ -0,0 +1,24 @@
+import Canvas from '../components/threeJS/canvas'
+import Menu from '../components/menu'
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+      <Menu/>
+      <Canvas/>
+    </div>
+  );
+}
+
+export default App;
