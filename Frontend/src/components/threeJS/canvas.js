5240a3ac4b449579af9dd333f13a15be1f707c75
Luca Pomer <43848253+LucaPomer@users.noreply.github.com>
2020-05-12 22:16:21 +0200

    added black canvas you can click on (#26)
    check the console log for x,y coordinates
    
    Co-authored-by: luca pomer <ucapomer@gmail.com>

--- Frontend/src/components/threeJS/canvas.js
+++ Frontend/src/components/threeJS/canvas.js
@@ -1,8 +1,49 @@
-import React from 'react';
-//import threeJs
+import React, {Component} from 'react';
+import * as THREE from 'three';
 
-function Canvas() {
-    return <div>Hello from Canvas</div>
+class Canvas extends Component {
+    componentDidMount() {
+        var scene = new THREE.Scene();
+        var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
+        var renderer = new THREE.WebGLRenderer();
+        renderer.setSize( window.innerWidth, window.innerHeight );
+        // use ref as a mount point of the Three.js scene instead of the document.body
+        this.mount.appendChild( renderer.domElement );
+        var geometry = new THREE.BoxGeometry( 1, 1, 1 );
+        var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+
+
+        let mouse = new THREE.Vector2();
+        function onMouseClick( event ) {
+
+            // calculate mouse position in normalized device coordinates
+            // (-1 to +1) for both components
+
+            mouse.x = ( event.clientX / window.innerWidth ) * 2 - 1;
+            mouse.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
+
+            //pay attention 0,0 is in the middle of the screen
+            console.log(`x  ${mouse.x} , y ${mouse.y} `);
+
+        }
+        window.addEventListener( 'click', onMouseClick, false );
+
+        var cube = new THREE.Mesh( geometry, material );
+        scene.add( cube );
+        camera.position.z = 5;
+        var animate = function () {
+            requestAnimationFrame( animate );
+            cube.rotation.x += 0.01;
+            cube.rotation.y += 0.01;
+            renderer.render( scene, camera );
+        };
+        animate();
+    }
+    render() {
+        return (
+            <div ref={ref => (this.mount = ref)} />
+        )
+    }
 }
 
 export default Canvas;
