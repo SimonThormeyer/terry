8181feca66154ec9ff2fc9227eca99c2644e43a2
Niklas Kramer <Niklas.kramer@me.com>
2020-05-14 18:33:12 +0200

    test_push

--- Frontend/src/containers/Parent.js
+++ Frontend/src/containers/Parent.js
@@ -1,30 +1,35 @@
 import React, {Component} from "react";
 import Menu from "../components/menu";
 import Canvas from "../components/threeJS/canvas";
+import {Synth} from '../components/toneJS/synth';
 
-class Parent extends Component{
+
+class Parent extends Component {
+    synth;
 
     constructor(props) {
         super(props);
+        this.synth = new Synth();
         this.updateXYCoordinates = this.updateXYCoordinates.bind(this);
         this.state = {
             xyCoordinates: []
         }
-
     }
 
-    updateXYCoordinates=(dataFromCanvas)=>{
-        console.log(dataFromCanvas[0] , dataFromCanvas[1]);
-        this.setState({ xyCoordinates:dataFromCanvas});
+    updateXYCoordinates = (dataFromCanvas) => {
+        this.synth.trigger("C4", "1n");
+        console.log(dataFromCanvas[0], dataFromCanvas[1]);
+        this.setState({xyCoordinates: dataFromCanvas});
     };
 
-    render(){
-        return(
-                <div>
+    render() {
+        return (
+            <div>
                 <Menu/>
-                <Canvas updateInfoParent = {this.updateXYCoordinates}/>
+                <Canvas updateInfoParent={this.updateXYCoordinates}/>
             </div>
         );
     }
 }
+
 export default Parent;
