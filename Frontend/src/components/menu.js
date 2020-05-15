88c1d465a242182930684a99007b79363e734604
chan <chantal.butenberg@gmail.com>
2020-05-15 17:00:56 +0200

    menu with icons ready
    icons as buttons change by click and trigger musicfunctions

--- Frontend/src/components/menu.js
+++ Frontend/src/components/menu.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 
 
+//import functions from app.js because of handle click 
+import { randomFunction, loopFunction, playFunction, recordFunction } from '../containers/App';
 
 import { ReactComponent as RandomIcon } from '../img/random.svg';
 import { ReactComponent as LooperIcon } from '../img/looper.svg';
@@ -8,10 +10,6 @@ import { ReactComponent as PlayIcon } from '../img/play.svg';
 import { ReactComponent as RecordIcon } from '../img/record.svg';
 import { ReactComponent as PauseIcon } from '../img/pause.svg';
 import { ReactComponent as StopIcon } from '../img/stop.svg';
-
-
-
-
 
 
 
@@ -28,27 +26,7 @@ class Menu extends React.Component {
 
     }
    
-
-    loopFunction(){
-        console.log("Looper")
-        this.props.functionCallFromParent("hello from child1");
-    }
-
-    randomFunction() {
-        console.log("randomfunction")
-        this.props.functionCallFromParent("hello from child1");
-    }
-
-    recordFunction() {
-        console.log("recordfunction")
-        this.props.functionCallFromParent("hello from child1");
-    }
-
-    playFunction() {
-        console.log("playfunction")
-        this.props.functionCallFromParent("hello from child1");
-    }
-
+//set State because of diffrent icons by click
     handleClick(stateID) {
         this.setState(prevState => ({
             [stateID]: !this.state[stateID],
@@ -57,28 +35,26 @@ class Menu extends React.Component {
 
 
    
-
-
-// FUNKTIONSFUNKTIONEN FEHLEN NOCH!
+//HTML structure with handleClick that toggles and call musicfunctions
     render() {
         return (
-            <ul id="buttonList">
-                <li id="playbutton" onClick={() => { this.handleClick("play"); this.playFunction()}}>
+            <ul id="menu">
+                <li id="playbutton" onClick={() => { this.handleClick("play"); playFunction() }}>
                     {this.state.play ?
                         <PlayIcon /> :
                         <PauseIcon />}
                 </li>
-                <li id="randombutton" onClick={() => { this.handleClick("random"); this.randomFunction() }}>
+                <li id="randombutton" onClick={() => { this.handleClick("random"); randomFunction() }}>
                     {this.state.random ?
                         <RandomIcon /> :
                         <PauseIcon />}
                 </li>
-                <li id="loopbutton" onClick={() => { this.handleClick("loop"); this.loopFunction() }}>
+                <li id="loopbutton" onClick={() => { this.handleClick("loop"); loopFunction() }}>
                     {this.state.loop ?
                         <LooperIcon /> :
                         <StopIcon />}
                 </li>
-                <li id="recordbutton" onClick={() => { this.handleClick("record"); this.recordFunction() }}>
+                <li id="recordbutton" onClick={() => { this.handleClick("record"); recordFunction() }}>
                     {this.state.record ?
                         <RecordIcon /> :
                         <StopIcon />}
