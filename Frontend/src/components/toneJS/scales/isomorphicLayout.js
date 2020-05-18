9af7c929b5df3699e05f897c9d684fa63aa63669
Niklas Kramer <Niklas.kramer@me.com>
2020-05-18 15:44:48 +0200

    - implemented function that maps note layouts to canvas

--- Frontend/src/components/toneJS/scales/isomorphicLayout.js
+++ Frontend/src/components/toneJS/scales/isomorphicLayout.js
@@ -2,11 +2,24 @@
 
 export class IsomorphicLayout{
 
-    // constructor() {
-    // }
+    constructor() {
+    this.scale = [
+        ["C5","D5","E5","G5","A5"],
+        ["C4","D4","E4","G4","A4"],
+        ["C3","D3","E3","G3","A3"],
+        ["C2","D2","E2","G2","A2"],
+        ["C1","D1","E1","G1","A1"]
+    ]
+     }
 
     coordinateToNote(valueX, valueY){
-        //console.log('ISO:' + valueX, valueY)
-        return "C4"
+        this.rows = this.scale.length
+        this.collumns = this.scale[0].length
+
+        this.x = Math.round((this.rows-1)* valueX)
+        this.y = Math.round((this.collumns-1) * valueY)
+
+        this.note = String(this.scale[this.y][this.x])
+        return this.note
     }
 }
