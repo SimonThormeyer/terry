58b750aa824f1955eece642f33853c98db70fb85
Niklas Kramer <Niklas.kramer@me.com>
2020-05-18 17:37:00 +0200

    added limiter

--- Frontend/src/components/toneJS/synth/synth.js
+++ Frontend/src/components/toneJS/synth/synth.js
@@ -3,7 +3,8 @@ import Tone from "tone";
 export class Synth {
 
     constructor() {
-        this.volume = new Tone.Volume(-8).toMaster();
+        this.limiter = new Tone.Limiter(-1).toMaster()
+        this.volume = new Tone.Volume(-12).connect(this.limiter);
         this.delay = new Tone.PingPongDelay(0.2, 0.7).connect(this.volume)
         this.filter = new Tone.Filter(400, 'lowpass', -12).connect(this.delay);
         this.polySynth = new Tone.PolySynth(8, Tone.FMSynth, {
