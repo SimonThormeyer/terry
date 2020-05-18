a743a9b2bd8668fa92fb69566b22d7bd9a1b39b1
Simon Thormeyer <49559340+SimonThormeyer@users.noreply.github.com>
2020-05-18 14:30:06 +0200

    fix sound problem (again) closes #38 (#53)
    fix sound problem caused by unnecessary rendering (again)
    
    Co-authored-by: Simon Thormeyer <simonthormeyer@gmail.com>

--- Frontend/src/containers/App.js
+++ Frontend/src/containers/App.js
@@ -1,28 +1,12 @@
 import Canvas from '../components/threeJS/canvas'
 import Menu from '../components/menu'
 import React, { useState, useMemo } from 'react';
-// import Parent from "./Parent";
 import { MusicCtrl } from '../components/toneJS/musicCtrl';
-
-//export functions for menu.js
-export const randomFunction = () => {
-  console.log("app js random Function");
-}
-export const loopFunction = () => {
-    console.log("app js loop Function");
-}
-  export const playFunction = () => {
-    console.log("app js play Function");
-  }
-  export const recordFunction = () => {
-    console.log("app js record Function");
-  }
 
 
 function App() {
 
-  const [musicCtrlParams, setMusicCtrlParams] = useState({ })
-  const [coordinates, setCoordinates] = useState([ ]);
+  const [musicCtrlParams, setMusicCtrlParams] = useState({})
 
   //only create a new MusicCtrl Object when parameters change that it depends on.
   let musicCtrl = useMemo(() => musicCtrlFactory(musicCtrlParams), [musicCtrlParams]);
@@ -36,15 +20,8 @@ function App() {
   const triggerToneWithCoordinates = (dataFromCanvas) => {
 
     musicCtrl.triggerSynth(dataFromCanvas[0], dataFromCanvas[1]);
-    setCoordinates(dataFromCanvas);
     console.log(`X: ${dataFromCanvas[0]}, Y: ${dataFromCanvas[1]}`);
-    console.log(`X from state: ${coordinates[0]}, Y from state: ${coordinates[1]}`);
   };
-
- 
- 
-
-
 
   return (
     <div className="App">
@@ -55,3 +32,17 @@ function App() {
 }
 
 export default App;
+
+//export functions for menu.js
+export const randomFunction = () => {
+  console.log("app js random Function");
+}
+export const loopFunction = () => {
+  console.log("app js loop Function");
+}
+export const playFunction = () => {
+  console.log("app js play Function");
+}
+export const recordFunction = () => {
+  console.log("app js record Function");
+}
