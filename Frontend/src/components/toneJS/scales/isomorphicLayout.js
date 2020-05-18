268ea6440bb23438ca67ea6fa978bb9bc163cd66
Niklas Kramer <Niklas.kramer@me.com>
2020-05-18 16:25:27 +0200

    - fucntion to rotate array needs to implemented

--- Frontend/src/components/toneJS/scales/isomorphicLayout.js
+++ Frontend/src/components/toneJS/scales/isomorphicLayout.js
@@ -1,25 +1,28 @@
-
-
-export class IsomorphicLayout{
+export class IsomorphicLayout {
 
     constructor() {
-    this.scale = [
-        ["C5","D5","E5","G5","A5"],
-        ["C4","D4","E4","G4","A4"],
-        ["C3","D3","E3","G3","A3"],
-        ["C2","D2","E2","G2","A2"],
-        ["C1","D1","E1","G1","A1"]
-    ]
-     }
+        this.penta1 = [
+            ["E4", "G4", "A4", "C5", "D5", "E5", "G5", "A5"],
+            ["A3", "C4", "D4", "E4", "G4", "A4", "C4", "D4"],
+            ["D3", "E3", "G3", "A3", "C4", "D4", "E4", "G4"],
+            ["G2", "A2", "C3", "D3", "E3", "G3", "A3", "C4"],
+            ["C2", "D2", "E2", "G2", "A2", "C3", "D3", "E3"]
+        ]
 
-    coordinateToNote(valueX, valueY){
+
+        this.scale = this.penta1
+    }
+
+    coordinateToNote(valueX, valueY) {
         this.rows = this.scale.length
         this.collumns = this.scale[0].length
+        console.log("ROWS: " + this.rows + " COLLUMNS: " + this.collumns)
+        this.x = Math.round((this.rows - 1) * (valueX))
+        this.y = Math.round((this.collumns - 1) * (valueY))
 
-        this.x = Math.round((this.rows-1)* valueX)
-        this.y = Math.round((this.collumns-1) * valueY)
+        this.note = this.scale[this.x][this.y]
+        console.log(this.note)
 
-        this.note = String(this.scale[this.y][this.x])
         return this.note
     }
 }
