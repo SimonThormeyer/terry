92ae375cd4e9f5f690177997151e84d76b3145d5
SimonThormeyer <simonthormeyer@gmail.com>
2020-05-19 13:45:39 +0200

    add GlobalState, clean up code

--- Frontend/src/components/menu.js
+++ Frontend/src/components/menu.js
@@ -1,66 +1,83 @@
-import React from 'react';
-
+import React, { useState } from 'react';
 import { ReactComponent as RandomIcon } from '../img/random.svg';
 import { ReactComponent as LooperIcon } from '../img/looper.svg';
 import { ReactComponent as PlayIcon } from '../img/play.svg';
 import { ReactComponent as RecordIcon } from '../img/record.svg';
 import { ReactComponent as PauseIcon } from '../img/pause.svg';
 import { ReactComponent as StopIcon } from '../img/stop.svg';
+import { useGlobalState } from "../GlobalState"
+import { Looper } from '../components/toneJS/Looper'
 
 
-
-class Menu extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            play: true,
-            loop: true, 
-            random: true,
-            record: true
-        };
+function Menu(props) {
     
+    // Global state - see GlobalState.js for explanation
+    const [listeningLooper, setListeningLooper] = useGlobalState('listeningLooper')
+    const [nextLooperID, setNextLooperID] = useGlobalState('nextLooperID'); 
+    const [runningLoopers, ] = useGlobalState('runningLoopers');
+    const [musicCtrl, ] = useGlobalState('musicCtrl');
+    
+    // state of Component 
+    const [play, setPlay] = useState(true)
+    const [random, setRandom] = useState(true)
+    const [loop, setLoop] = useState(true)
+    const [record, setRecord] = useState(true)
 
-    }
-   
-//set State because of diffrent icons by click
-    handleClick(stateID) {
-        this.setState(prevState => ({
-            [stateID]: !this.state[stateID],
-        }));
+
+    const loopFunction = (startLoop) => {
+        if (startLoop) {
+            setListeningLooper(new Looper(Date.now(), musicCtrl));
+        } else {
+            listeningLooper.stopRecording(Date.now());
+            listeningLooper.play();
+            setNextLooperID(nextLooperID + 1);
+            runningLoopers.set(nextLooperID, listeningLooper);
+            setListeningLooper(undefined)
+        }
     }
 
-
-   
-//HTML structure with handleClick that toggles and call musicfunctions
-    render() {
-        let loopFunctionParameter = this.state.loop ? "start" : "stop";
-        return (
-            <ul id="menu">
-                <li id="playbutton" onClick={() => { this.handleClick("play"); this.props.playFunction(); }}>
-                    {this.state.play ?
-                        <PlayIcon /> :
-                        <PauseIcon />}
-                </li>
-                <li id="randombutton" onClick={() => { this.handleClick("random"); this.props.randomFunction() }}>
-                    {this.state.random ?
-                        <RandomIcon /> :
-                        <PauseIcon />}
-                </li>
-                <li id="loopbutton" onClick={() => { this.handleClick("loop"); this.props.loopFunction(loopFunctionParameter) }}>
-                    {this.state.loop ?
-                        <LooperIcon /> :
-                        <StopIcon />}
-                </li>
-                <li id="recordbutton" onClick={() => { this.handleClick("record"); this.props.recordFunction() }}>
-                    {this.state.record ?
-                        <RecordIcon /> :
-                        <StopIcon />}
-                </li>
-            </ul>
-        );
+    const playFunction = () =>
+    {
+        console.log("menu js play Function");
     }
+
+    const randomFunction = () =>
+    {
+        console.log("menu js random Function");
+    }
+
+    const recordFunction = () =>
+    {
+        console.log("menu js record Function");
+    }
+
+    return (
+        <ul id="menu">
+            <li id="playbutton" onClick={() => { setPlay(!play); playFunction(); }}>
+                {play ?
+                    <PlayIcon /> :
+                    <PauseIcon />}
+            </li>
+            <li id="randombutton" onClick={() => { setRandom(!random); randomFunction() }}>
+                {random ?
+                    <RandomIcon /> :
+                    <PauseIcon />}
+            </li>
+            <li id="loopbutton" onClick={() => {
+                loopFunction(loop);
+                setLoop(!loop);
+            }}>
+                {loop ?
+                    <LooperIcon /> :
+                    <StopIcon />}
+            </li>
+            <li id="recordbutton" onClick={() => { setRecord(!record); recordFunction() }}>
+                {record ?
+                    <RecordIcon /> :
+                    <StopIcon />}
+            </li>
+        </ul>
+    );
 }
-
-
 
 export default Menu;
--- Frontend/src/containers/App.js
+++ Frontend/src/containers/App.js
@@ -1,72 +1,21 @@
 import Canvas from '../components/threeJS/canvas'
 import Menu from '../components/menu'
-import React, { useState, useMemo } from 'react';
-import { MusicCtrl } from '../components/toneJS/musicCtrl';
-import { Looper } from '../components/toneJS/Looper'
+import React from 'react';
+import { useGlobalState } from "../GlobalState"
 
 
 function App() {
 
-  const [musicCtrlParams, setMusicCtrlParams] = useState({})
-  const [looper, setLooper] = useState({
-    isRecording: false,
-    looper: undefined
-  })
+  const [listeningLooper,] = useGlobalState('listeningLooper')
+  const [runningLoopers, setRunningLoopers] = useGlobalState('runningLoopers');
+  const [musicCtrl,] = useGlobalState('musicCtrl');
 
-  ///temporary, until there is a real "loopers" component
-  const [looperCount, setLooperCount] = useState(1);
-  const [runningLoopers, setRunningLoopers] = useState(new Map());
-  let loopersMap = runningLoopers;
-  
-  //only create a new MusicCtrl Object when parameters change that it depends on.
-  let musicCtrl = useMemo(() => musicCtrlFactory(musicCtrlParams), [musicCtrlParams]);
-
-  //returns a new MusicCtrl Object depending on the parameters
-  function musicCtrlFactory(params) {
-    return new MusicCtrl()
-  }
-
-  // Callback Functions for Menu ============
-  const randomFunction = () => {
-    console.log("app js random Function");
-  }
-  const loopFunction = (parameter) => {
-    if (parameter === "start") {
-      console.log("app js loop Function " + parameter);
-      let looper = new Looper(Date.now(), musicCtrl);
-      setLooper({
-        isRecording: true,
-        looper: looper
-      })
-    } else {
-      console.log("app js loop Function " + parameter);
-      looper.looper.stopRecording(Date.now());
-      looper.looper.play();
-      setLooperCount(looperCount + 1);
-      loopersMap.set(looperCount, looper.looper);
-      setRunningLoopers(loopersMap);
-      setRunningLoopers(new Map(loopersMap));
-      setLooper({
-        isRecording: false,
-        looper: undefined
-      })
-    }
-  }
-  const playFunction = () => {
-    musicCtrl.startStopSoundbed()
-    console.log("app js play Function");
-  }
-  const recordFunction = () => {
-    console.log("app js record Function");
-  }
-  // ========================================
-
-  //Callbacks for Canvas ====================
+  // ==================== Callbacks for Canvas (could be transferred to Canvas using GlobalState) 
   const triggerToneWithCoordinates = (dataFromCanvas) => {
     musicCtrl.triggerSynth(dataFromCanvas[0], dataFromCanvas[1]);
     console.log(`X: ${dataFromCanvas[0]}, Y: ${dataFromCanvas[1]}`);
-    if (looper.isRecording) {
-      looper.looper.addEvents(
+    if (listeningLooper) { //if there is a looper currently recording actions
+      listeningLooper.addEvents(
         {
           timestamp: Date.now(),
           type: "canvasClick",
@@ -78,27 +27,29 @@ function App() {
   };
 
   const recordMouseMovement = (eventArray) => {
-    if (looper.isRecording) {
-      looper.looper.addEvents(eventArray);
+    if (listeningLooper) { //if there is a looper currently recording actions
+      listeningLooper.addEvents(eventArray);
     }
   }
-
-  // ========================================
+  // ======================================== (End callbacks canvas)
 
 
   return (
     <div className="App">
-      {Array.from(loopersMap.keys()).map((id) => {
+      {/* This should be part of some Looper-Controls-Component in the future (=> Luca) */}
+      {Array.from(runningLoopers.keys()).map((id) => {
+        // iterate over elements in runningLoopers and create a button for each
         return (
           <button key={`loopStopButton_${id}`} onClick={() => {
-            loopersMap.get(id).stop()
-            loopersMap.delete(id);
-            setRunningLoopers(new Map(loopersMap));
+            runningLoopers.get(id).stop()
+            runningLoopers.delete(id);
+            setRunningLoopers(new Map(runningLoopers));
           }}>Stop Looper {id}</button>
         )
       })}
+      {/* End of Looper-Controls */}
       <Canvas id={"canvas"} updateInfoParent={triggerToneWithCoordinates} recordMouseMovement={recordMouseMovement} />
-      <Menu randomFunction={randomFunction} loopFunction={loopFunction} playFunction={playFunction} recordFunction={recordFunction} />
+      <Menu/>
     </div>
   );
 }
