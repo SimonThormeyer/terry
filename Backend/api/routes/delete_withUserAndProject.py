efab38e0855a7966fe13e4efeb7cc18060dd78ac
Maluna <maluna.menke@web.de>
2020-05-26 19:19:06 +0200

    rechanging docker files for hot loading and reworking @ backend/routes

--- Backend/api/routes/delete_withUserAndProject.py
+++ Backend/api/routes/delete_withUserAndProject.py
@@ -2,7 +2,7 @@ from .responses import Standard200Response
 from . import projects
 
 
-@projects.route('/remove/<user_ID>/<project_name>', methods=['DELETE'])
+@projects.route('/remove/user/<user_ID>/project/<project_name>', methods=['DELETE'])
 def delete_withUserAndProject(user_id, project_name):
     # implement DB query etc
     res = {'User_ID': user_id,
