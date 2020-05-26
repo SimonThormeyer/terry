31bc4126fd11e7caa335d4dc1cfc05ec9e3d6801
Maluna <maluna.menke@web.de>
2020-05-26 12:04:10 +0200

    fixing import bugs and adding a testroute and adding code change detection for backend

--- Backend/api/routes/delete_withUserAndProject.py
+++ Backend/api/routes/delete_withUserAndProject.py
@@ -1,9 +1,8 @@
-from api import app
 from .responses import Standard200Response
 from . import projects
 
 
-@projects.route('/<user_ID>/<project_name>', methods=['DELETE'])
+@projects.route('/remove/<user_ID>/<project_name>', methods=['DELETE'])
 def delete_withUserAndProject(user_id, project_name):
     # implement DB query etc
     res = {'User_ID': user_id,
