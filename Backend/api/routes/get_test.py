9e0c2ffddbfbeb871b8d392c023c66806bce7949
fr3ddykrueger <64377121+fr3ddykrueger@users.noreply.github.com>
2020-05-27 11:42:21 +0200

    Revert "Ticket/docker compose files (#85)" (#88)
    This reverts commit f93929ff085e0fa626f50910a0529669228e9f57.
    
    Co-authored-by: Simon Thormeyer <49559340+SimonThormeyer@users.noreply.github.com>

--- Backend/api/routes/get_test.py
+++
@@ -1,8 +0,0 @@
-from .responses import Standard200Response
-from . import projects
-
-
-@projects.route('/test', methods=['GET'])
-def test():
-    res = {'Res': 'Test was successful!'}
-    return Standard200Response(res)
