940dd0f1ca22d4741487e5594ed274359e896f5c
lunarie16 <47414934+lunarie16@users.noreply.github.com>
2020-05-27 22:13:18 +0200

    Ticket/docker compose files (#87)
    * fixing import bugs and adding a testroute and adding code change detection for backend
    
    * rechanging docker files for hot loading and reworking @ backend/routes
    
    Co-authored-by: Simon Thormeyer <49559340+SimonThormeyer@users.noreply.github.com>

--- Backend/api/routes/__init__.py
+++ Backend/api/routes/__init__.py
@@ -6,3 +6,4 @@ from .get_withProjectName import get_withProjectName
 from .get_withUser import get_withUser
 from .get_withUserAndProject import get_withUserAndProject
 from .post_withUserAndProject import post_withUserAndProject
+from .get_test import test
