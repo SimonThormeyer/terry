d88d3623b06e7228e365248367b0b00c750f5a7e
Simon Thormeyer <49559340+SimonThormeyer@users.noreply.github.com>
2020-05-27 11:39:08 +0200

    Revert "Ticket/docker compose files (#85)"
    This reverts commit f93929ff085e0fa626f50910a0529669228e9f57.

--- Backend/api/routes/get_withUserAndProject.py
+++ Backend/api/routes/get_withUserAndProject.py
@@ -1,8 +1,9 @@
+from api import app
 from .responses import Standard200Response
 from . import projects
 
 
-@projects.route('/user/<user_ID>/project/<project_name>', methods=['GET'])
+@projects.route('/<user_ID>/<project_name>', methods=['GET'])
 def get_withUserAndProject(user_ID, project_name):
     # implement DB query etc
     res = {'User_ID': user_ID,
