73bd86bf89058c5a6fc727539ee48188653958cf
Simon Thormeyer <49559340+SimonThormeyer@users.noreply.github.com>
2020-05-27 22:02:15 +0200

    Canvas looper bug#86 luca simon (#90)
    * canvas added references
    
    * added log
    
    * try fix looper bug..
    
    * fix looper bug :wrench:
    
    Co-authored-by: luca pomer <ucapomer@gmail.com>
    Co-authored-by: SimonThormeyer <simonthormeyer@gmail.com>

--- Frontend/src/containers/App.js
+++ Frontend/src/containers/App.js
@@ -9,35 +9,6 @@ function App() {
   //can be reomved as soon as there is a 'loopers control' component (see below, used to be able to stop the loopers and create corresponding buttons)
   const [runningLoopers, setRunningLoopers] = useGlobalState('runningLoopers');
 
-  // can be removed as soon as canvas doesn't need callbacks anymore
-  const [musicCtrl,] = useGlobalState('musicCtrl');
-  const [listeningLooper,] = useGlobalState('listeningLooper')
-
-
-  // ==================== Callbacks for Canvas (can be transferred to Canvas as soon as it is able to use GlobalState) 
-  const triggerToneWithCoordinates = (dataFromCanvas) => {
-    musicCtrl.triggerSynth(dataFromCanvas[0], dataFromCanvas[1]);
-    console.log(`X: ${dataFromCanvas[0]}, Y: ${dataFromCanvas[1]}`);
-    if (listeningLooper) { //if there is a looper currently recording actions
-      listeningLooper.addEvents(
-        {
-          timestamp: performance.now(),
-          type: "canvasClick",
-          x: dataFromCanvas[0],
-          y: dataFromCanvas[1]
-        }
-      )
-    }
-  };
-
-  const recordMouseMovement = (eventArray) => {
-    if (listeningLooper) { //if there is a looper currently recording actions
-      listeningLooper.addEvents(eventArray);
-    }
-  }
-  // ======================================== (End callbacks canvas)
-
-
   return (
     <div className="App">
       {/* This should be part of some "Looper-Controls-Component" in the future (=> Luca) */}
@@ -63,7 +34,7 @@ function App() {
         )
       })}
       {/* End of "Looper-Controls" */}
-      <Canvas updateInfoParent={triggerToneWithCoordinates} recordMouseMovement={recordMouseMovement} />
+      <Canvas />
       <Menu />
     </div>
   );
