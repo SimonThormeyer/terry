c5c8fc838f0b5110f2ce11cafb0b3f5933feb3f4
Niklas Kramer <Niklas.kramer@me.com>
2020-05-28 10:12:38 +0200

    added class for effects

--- Frontend/src/components/toneJS/effects/effects.js
+++ Frontend/src/components/toneJS/effects/effects.js
@@ -6,18 +6,14 @@ export class Effects {
 
         this.limiter = new Tone.Limiter(-1).toMaster()
         this.volume = new Tone.Volume(-12).connect(this.limiter);
-        this.delay = new Tone.PingPongDelay().connect(this.volume)
-        this.start = new Tone.Volume(-12).connect(this.limiter);
+        this.delay = new Tone.PingPongDelay(0.1,0.8).connect(this.volume)
+        this.start = new Tone.Volume(-12).connect(this.delay);
 
     }
 
     setDelay(value) {
-        this.delay = new Tone.PingPongDelay(8n).chain(this.volume)
-        this.delay.wet.value = (value * 50) + 50
-        //this.delay.set("wet",(value * 50) + 50)
-        //console.log((value * 50) + 50)
-
-        //console.log(this.delay)
+        this.delay.wet.value = ((value + 1)/2)
+        console.log(this.delay.wet.value)
 
         //this.delay.set('feedback', this.feebackValue);
         //this.delay.set('wet',value)
