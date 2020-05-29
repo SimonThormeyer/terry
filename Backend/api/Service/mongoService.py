09b86fe3ef517d9330f6b9e401f0497e8725ea9c
Maluna <maluna.menke@web.de>
2020-05-29 17:21:13 +0200

    implementing controller and service for mongoDB querys

---
+++ Backend/api/Service/mongoService.py
@@ -0,0 +1,46 @@
+from Backend.api import proj_col
+
+
+def insert_ProjectWithProjectNameFromUser(user_ID,project_name):
+    proj = {"project_name": project_name,
+            "user_ID": user_ID}
+    res = proj_col.insert_one(proj)
+    return res 
+
+def query_ProjectsFromUser(user_ID):
+    query = {"user_ID": user_ID}
+    res = proj_col.find(query)
+    return res 
+
+def query_ProjectWithProjectName(project_name):
+    query = {"project_name": project_name}
+    res = proj_col.find(query)
+    return res 
+
+def query_ProjectWithProjectNameFromUser(user_ID,project_name):
+    query = {"project_name": project_name,
+            "user_ID": user_ID}
+    res = proj_col.find(query)
+    return res 
+
+def delete_ProjectWithProjectNameFromUser(user_ID,project_name):
+    query = {"project_name": project_name,
+            "user_ID": user_ID}
+    res = proj_col.delete_one(query)
+    return res 
+
+
+# TODO: projekt muss hier noch mit rein 
+def update_ProjectWithProjectNameFromUser(user_ID,project_name, newUser_ID, newProject_name):
+    query = {"project_name": project_name,
+            "user_ID": user_ID}
+    newValue = { "$set": {"project_name": project_name,
+            "user_ID": user_ID} }      
+
+# idk ob wir das brauchen
+def query_AllProjects():
+    return proj_col.find()
+
+
+# Limit results with .limit()
+# myresult = mycol.find().limit(5)
--- Backend/api/__init__.py
+++ Backend/api/__init__.py
@@ -1,10 +1,20 @@
 from flask import Flask
+from flask_pymongo import PyMongo
 from flask_cors import CORS
 import os
-import pymongo
 
 app = Flask(__name__)
 CORS(app)
+
+app.config['MONGO_URI'] = 'mongodb://localhost:27017/database'
+app.config['Mongo_DBNAME'] = 'Projects'
+app.config['SECRET_KEY'] = 'secret_key'
+
+mongo = PyMongo(app)
+db = mongo.db
+proj_col = db['Projects']
+
+
 
 from api.routes import projects
 app.register_blueprint(projects, url_prefix=os.environ['URL_PREFIX'])
