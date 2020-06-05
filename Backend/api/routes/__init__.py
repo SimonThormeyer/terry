6ee593f8cccd633cedf143a7b48a66c5919750a2
fr3ddykrueger <64377121+fr3ddykrueger@users.noreply.github.com>
2020-06-05 09:50:46 +0200

    Ticket/#49/init mongo (#102)
    * added Folders
    
    * combining api-routes in one file
    
    * change docker
    
    * adding mongodb to docker-compose
    
    * implementing controller and service for mongoDB querys
    
    * project is now extracted from request body as json
    
    * added venv in gitignore
    
    * integrated mongoengine and init the db-model
    
    * updated dynamic doc in models
    
    * fixing import errors in Backend
    
    * updated folder names
    
    * updated folder names
    
    * renaming controller folder
    
    * renaming folders
    
    * fixing all mongoDB bugs
    
    * deleting unnessary file
    
    * adding reverse respond, so newest is first
    
    * created and deleted comments
    
    * added Folders
    
    * combining api-routes in one file
    
    * change docker
    
    * adding mongodb to docker-compose
    
    * implementing controller and service for mongoDB querys
    
    * project is now extracted from request body as json
    
    * added venv in gitignore
    
    * integrated mongoengine and init the db-model
    
    * updated dynamic doc in models
    
    * fixing import errors in Backend
    
    * updated folder names
    
    * renaming controller folder
    
    * renaming folders
    
    * fixing all mongoDB bugs
    
    * deleting unnessary file
    
    * adding reverse respond, so newest is first
    
    * created and deleted comments
    
    * now not posib to have user_id and more than one project with same name
    
    * fixing logic errors (checking if project name with this user already in use)
    
    * making code pretty and updating timezone to local time and pretty format
    
    Co-authored-by: Maluna <maluna.menke@web.de>

--- Backend/api/routes/__init__.py
+++ Backend/api/routes/__init__.py
@@ -1,9 +1,4 @@
 from flask import Blueprint
 projects = Blueprint('projects', __name__)
 
-from .delete_withUserAndProject import delete_withUserAndProject
-from .get_withProjectName import get_withProjectName
-from .get_withUser import get_withUser
-from .get_withUserAndProject import get_withUserAndProject
-from .post_withUserAndProject import post_withUserAndProject
-from .get_test import test
+from .routes import delete_withUserAndProject, get_withProjectName, get_withUser, get_withUserAndProject, post_withUserAndProject, test
