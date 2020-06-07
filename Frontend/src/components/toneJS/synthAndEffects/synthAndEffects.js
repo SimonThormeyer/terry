b6cce814106563fc741bbff587cb6f30ba29db17
NiklasKramer <43847968+NiklasKramer@users.noreply.github.com>
2020-06-07 15:04:30 +0200

    #92 complex parameter changes niklas (#111)
    * added class for effects
    
    * added class for effects
    
    * add pause and mute functionality to looper (#84)
    
    * mute is working, pause is buggy
    
    * start debugging pause with console prints
    
    * debugging looper.pause
    
    * fix pause (?)
    
    * remove setInterval, use performance.now() instead of Date.now()
    
    * clean up
    
    close #80
    
    Co-authored-by: Simon Thormeyer <simonthormeyer@gmail.com>
    
    * Revert "Ticket/docker compose files (#85)" (#88)
    
    This reverts commit f93929ff085e0fa626f50910a0529669228e9f57.
    
    Co-authored-by: Simon Thormeyer <49559340+SimonThormeyer@users.noreply.github.com>
    
    * Canvas looper bug#86 luca simon (#90)
    
    * canvas added references
    
    * added log
    
    * try fix looper bug..
    
    * fix looper bug :wrench:
    
    Co-authored-by: luca pomer <ucapomer@gmail.com>
    Co-authored-by: SimonThormeyer <simonthormeyer@gmail.com>
    
    * Ticket/docker compose files (#87)
    
    * fixing import bugs and adding a testroute and adding code change detection for backend
    
    * rechanging docker files for hot loading and reworking @ backend/routes
    
    Co-authored-by: Simon Thormeyer <49559340+SimonThormeyer@users.noreply.github.com>
    
    * added class for effects
    
    * added class for effects
    
    * added class for effects
    
    * added class for effects
    
    * initial commit
    
    * some refactoring
    
    * some refactoring
    
    * some refactoring
    
    * more effects
    
    * more effects
    
    * initial commit
    
    * some refactoring
    
    * some refactoring
    
    * some refactoring
    
    * more effects
    
    * more effects
    
    * more effects
    
    Co-authored-by: Simon Thormeyer <49559340+SimonThormeyer@users.noreply.github.com>
    Co-authored-by: Simon Thormeyer <simonthormeyer@gmail.com>
    Co-authored-by: fr3ddykrueger <64377121+fr3ddykrueger@users.noreply.github.com>
    Co-authored-by: luca pomer <ucapomer@gmail.com>
    Co-authored-by: lunarie16 <47414934+lunarie16@users.noreply.github.com>

--- Frontend/src/components/toneJS/synthAndEffects/synthAndEffects.js
+++ Frontend/src/components/toneJS/synthAndEffects/synthAndEffects.js
@@ -5,15 +5,19 @@ export class SynthAndEffects {
     constructor() {
 
         // Settings
-        this.noteLengthOptions = ["32n", "16n", "8n", "4n", "2n", "1n"]
-        this.waveforms = ["sine", "saw", "pulse"]
+        this.noteLengthOptions = ["64n", "32n", "16n", "8n", "4n", "2n", "1n"]
+        this.waveforms = ["sine6", "triangle6"]
         this.noteLength = this.noteLengthOptions[2]
+
 
         // INSTRUMENT_CHAIN
         //Effects
         this.limiter = new Tone.Limiter(-1).toMaster()
-        this.volume = new Tone.Volume(-12).connect(this.limiter);
-        this.delay = new Tone.PingPongDelay(0.1, 0).connect(this.volume)
+        this.volume = new Tone.Volume(-5).connect(this.limiter);
+        this.reverb = new Tone.Reverb(2).connect(this.volume)
+        this.pan = new Tone.Panner(1).connect(this.reverb)
+        this.delay = new Tone.PingPongDelay(0.1, 0).connect(this.pan)
+
 
         //Synth
         this.filter = new Tone.Filter(400, 'lowpass', -12).connect(this.delay)
@@ -22,6 +26,19 @@ export class SynthAndEffects {
                 type: "sine",
             }
         }).connect(this.filter);
+
+        //LFOs
+        this.panLfo = new Tone.LFO(5, 0, 1);
+        this.panLfo.connect(this.pan.pan);
+        this.panLfo.start()
+
+        //UTILITY
+        this.delayCounter = 0
+        this.reverbCounter = 0
+
+        // INITIALISING
+        this.reverb.generate() //reverb needs to be initialised
+        this.reverb.wet.value = 0.1
     }
 
     //SYNTH FUNCTIONS
@@ -29,37 +46,76 @@ export class SynthAndEffects {
         this.polySynth.triggerAttackRelease(note, this.noteLength);
     }
 
-    setFilter(value) {
-        let calculatedFrequency = (value + 1) / 2 * 5000
-        // console.log("BEFORE: " + this.filter.frequency.value)
+    setFilter(valueX, valueY) {
+        let calculatedFrequency = (this._normalizeRange(valueX) * 1300) + 200
         this.filter.frequency.value = calculatedFrequency
-        // console.log("AFTER: " + this.filter.frequency.value)
+        // compensate volume when the filter opens up
+        this.volume.volume.value = ((-1) * (this._normalizeRange(valueX)) * 5) - 5
+
     }
 
     setNoteLength(value) {
         let numberOfLengthOptions = this.noteLengthOptions.length - 1
         let position = Math.round(((value + 1) / 2) * numberOfLengthOptions)
         this.noteLength = this.noteLengthOptions[position]
-        // console.log("Notelength: " + position)
+
+    }
+
+    setSynthADSR(value) {
+        this.polySynth.set({
+            "envelope": {
+                "sustain": (this._normalizeRange(value) * 0.9) + 0.1,
+                "attack": (this._normalizeRange(value) * 0.2)
+            }
+        });
+    }
+
+    setOscillatorType(value) {
+        let numberOfWaveformOptions = this.waveforms.length - 1
+        let position = Math.round(((value + 1) / 2) * numberOfWaveformOptions)
+        let waveform = this.waveforms[position]
+        this.polySynth.set(
+            {
+                oscillator: {
+                    type: waveform
+
+                }
+            })
     }
 
 
     // EFFECT FUNCTIONS
+    //Panning
+    setPanningEffect(valueX, valueY) {
+        this.panLfo.set("max", this._normalizeRange(valueX)*this._normalizeRange(valueX))
+        this.panLfo.frequency.value = this._normalizeRange(valueY) * 8
+
+    }
+
+    //Delay
     setDelayFeedback(value) {
-        console.log(this.delayCounter)
-        //set between 0 - 0.9 to avoid endless feedback
-        let valueIntoNormalRange = (((value + 1) / 2)*0.9)
-        // console.log("value: " + valueIntoNormalRange)
-        this.delay.feedback.value = valueIntoNormalRange
-        // console.log("FEEDBACK: " + this.delay.wet.value)
+        if (this.delayCounter % 35) {
+            this.delay.feedback.value = (this._normalizeRange(value)) * 0.9
+        }
+        this.delayCounter++
     }
 
     setDelayWet(value) {
-        console.log(this.delayCounter)
-        let valueIntoNormalRange = (((value + 1) / 2))
-        // console.log("value: " + valueIntoNormalRange)
-        this.delay.wet.value = valueIntoNormalRange
-        // console.log("FEEDBACK: " + this.delay.wet.value)
+        this.delay.wet.value = this._normalizeRange(value)
+    }
+
+    //Reverb
+    setReverbWet(value) {
+        if (this.reverbCounter % 50) {
+            this.reverb.wet.value = (this._normalizeRange(value)) * 0.9
+        }
+        this.reverbCounter++
+    }
+
+
+    //INTERNAL FUNCTIONS
+    _normalizeRange(value) {
+        return ((value + 1) / 2)
     }
 
 }
