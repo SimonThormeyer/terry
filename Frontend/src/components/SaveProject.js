652be631dad770a4b16455cf01c44d5be1d97665
chanty1512 <43848248+chanty1512@users.noreply.github.com>
2020-06-12 10:28:17 +0200

    Ticket/97/send global state to api simon chantal (#114)
    * json containing looper info is generated
    
    also removing nextLooperID from GlobalState
    adding a saveButton Component
    adding a proxy to flask api package.json
    
    * switch from fetch to axios, adjust looper interface
    
    * looper does not need musicCtrl or startTime anymore when instantiated
    
    * side menu + save overlay
    
    * minimize area for loopcontrol
    
    * minimize area for loopcontrols
    
    * openProject visuell
    
    * saving project in database works!
    
    * get list from database and find project works
    
    * placeholder function for reconstruktor project by clicking on a projectname
    
    * improved functionality, more beautiful code, sideMenu save and open dialogue
    
    save functionality in database
    
    * corrected alert messages for save/open project
    
    Co-authored-by: SimonThormeyer <simonthormeyer@gmail.com>

---
+++ Frontend/src/components/SaveProject.js
@@ -0,0 +1,44 @@
+import axios from 'axios';
+
+
+    
+function SaveProject(loopers, username, projectname) {
+
+        const runningLoopers = loopers;
+         
+        let userID = username;
+        let projectName = projectname;
+        let backendUrl = `http://localhost:5000`;
+
+        let getGlobalState = () => {
+            let loopers = [];
+            for (let looper of Array.from(runningLoopers.values())) {
+                loopers.push(looper.getLooper());
+            }
+            return {
+                loopers: loopers
+            }
+        }
+        
+        axios
+
+            // Anfang POST
+            .post(`${backendUrl}/projects/user/${userID}/project/${projectName}`, getGlobalState())
+            .then(res => {
+                console.log(`successful: ${res.statusText}`);
+                alert("Saved successfully");
+            }) 
+            // Ende Post
+
+            .catch(err => {
+                err.response ? console.log(`error: ${err.response.data}`) : console.log(err.message);
+                alert("Oooops, project name already in use");
+
+            })
+
+    
+   
+ }
+
+
+export default SaveProject;
