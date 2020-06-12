aeee4448cc77f419830f79a9f0371f7e9e921dc9
chanty1512 <43848248+chanty1512@users.noreply.github.com>
2020-06-12 10:28:17 +0200

    Ticket/97/send global state to api simon chantal (#114)
    * json containing looper info is generated
    
    also removing nextLooperID from GlobalState
    adding a saveButton Component
    adding a proxy to flask api package.json
    
    * switch from fetch to axios, adjust looper interface
    
    * looper does not need musicCtrl or startTime anymore when instantiated
    
    * side menu + save overlay
    
    * minimize area for loopcontrol
    
    * minimize area for loopcontrols
    
    * openProject visuell
    
    * saving project in database works!
    
    * get list from database and find project works
    
    * placeholder function for reconstruktor project by clicking on a projectname
    
    * improved functionality, more beautiful code, sideMenu save and open dialogue
    
    save functionality in database
    
    * corrected alert messages for save/open project
    
    Co-authored-by: SimonThormeyer <simonthormeyer@gmail.com>

--- Frontend/src/containers/App.js
+++ Frontend/src/containers/App.js
@@ -2,12 +2,14 @@ import Canvas from '../components/threeJS/canvas'
 import Menu from '../components/menu'
 import React from 'react';
 import Loopcontrols from "./loopcontrols";
+import SideMenu from '../components/sidemenu';
 
 
 function App() {
 
   return (
     <div className="App">
+      <SideMenu />
       <Loopcontrols />
       <Canvas />
       <Menu />
