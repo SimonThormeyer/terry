a9910838da1882f13e95e80f06871182d3630b14
chanty1512 <43848248+chanty1512@users.noreply.github.com>
2020-06-14 17:14:59 +0200

    Ticket/#120/help dialogue chantal (#146)
    * html return, states and css
    
    * helpDialogue is done
    
    * fix warning messages
    
    Co-authored-by: SimonThormeyer <simonthormeyer@gmail.com>

--- Frontend/src/GlobalState.js
+++ Frontend/src/GlobalState.js
@@ -19,6 +19,7 @@ const initialState = {
     musicCtrl: undefined,
     overlayIsOpen: false,
     globalFunctions: {},
+    activeHelpDialogue : "",
 };
 
 export const { useGlobalState } = createGlobalState(initialState)
