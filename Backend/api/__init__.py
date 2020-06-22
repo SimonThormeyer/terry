4ce436734876d97a329177581100262a87487930
Jean-Frederic Alscher <fredericalscher@yahoo.de>
2020-06-22 19:58:22 +0200

    done rebase

--- Backend/api/__init__.py
+++ Backend/api/__init__.py
@@ -13,34 +13,7 @@ CORS(app)
 #mongo_host = os.environ['MONGO_HOST']
 mongo = PyMongo(app, uri='mongodb://mongo:27017/Projects')
 db = mongo.db
-
-
-
-log.info('list collections')
-for x in db.list_collection_names():
-  log.info(len(x))
-  log.info(str(x))
-log.info(str(db.list_collection_names()))
-
-# client = MongoClient()
-# db2 = client.projects
-#
-# log.info(client.list_databases())
-# log.info(db2.list_collection_names())
-#db2.drop_collection('Projects')
-#proj_col = db['Projects']
-
-# client = pymongo.MongoClient("localhost:27017")
-# log.info('Client '+ str(client))
-# db = client["projects"]
-# log.info('DB '+str(db))
-# mycol = db["project"]
-# log.info("List of collections \n--------------------------")
-# for x in db.list_collection_names():
-#   log.info(len(x))
-#   log.info(x)
-
-
+proj_col = db['Projects']
 ma = Marshmallow(app)
 
 
--- Backend/api/routes/routes.py
+++ Backend/api/routes/routes.py
@@ -44,8 +44,6 @@ def testdb():
         log.info(validateProject(project))
         return Standard400ErrorResponse('Oooops, This Request is not valid', validateProject(project))
 
-
-
 @projects.route('/all', methods=['GET'])
 def get_allProjects():
     temp = Service.query_AllProjects()
@@ -64,9 +62,8 @@ def get_allProjects():
             log.info(validateProject(project))
             return Standard400ErrorResponse('Oooops, This Request is not valid', validateProject(project))
     res['items'] = projects
-    res['self'] = url_for('get_allProjects')
+    res['self'] = url_for('projects.get_allProjects')
     return Standard200Response(res)
-
 
 @projects.route('/user/<user_ID>', methods=['GET'])
 def getUser(user_ID):
@@ -82,14 +79,13 @@ def getUser(user_ID):
         log.info(validateUser(res))
         return Standard400ErrorResponse('Oooops, This Request is not valid', validateUser(user))
 
-
 @projects.route('user/<user_ID>/allprojects')
 def get_withUser(user_ID):
     temp  = Service.query_ProjectsFromUser(user_ID)
     projects = []
     res = {}
     if temp.count == 0:
-        return responses.Standard404ErrorResponse
+        return responses.Standard404ErrorResponse()
     for project in temp:
         try:
             projects.append(generateProjectLinks(project))
@@ -101,25 +97,43 @@ def get_withUser(user_ID):
             log.info(validateProject(project))
             return Standard400ErrorResponse('Oooops, This Request is not valid', validateProject(project))
     res['items'] = projects
-    res['links'] = {'self' : url_for('projects.get_withUser', user_ID=user_ID)}
+    res['self'] = url_for('projects.get_withUser', user_ID=user_ID)
     return Standard200Response(res)
-
-
-
-
 
 @projects.route('/project/<project_name>', methods=['GET'])
 def get_withProjectName(project_name):
-    res = Service.query_ProjectWithProjectName(project_name)
-    if res.count == 0:
-        return responses.Standard404ErrorResponse
+    temp = Service.query_ProjectWithProjectName(project_name)
+    log.info('route get_withProjectName')
+    projects = []
+    res = {}
+    if temp.count == 0:
+        return responses.Standard404ErrorResponse()
+    for project in temp:
+        try:
+            log.info(project)
+            projects.append(generateProjectLinks(project))
+        except ValidationError as e:
+            log.info('validator.errors')
+            log.info('this data is correct')
+            log.info(e.valid_data)
+            log.info('this data is not correct')
+            log.info(validateProject(project))
+            return Standard400ErrorResponse('Oooops, This Request is not valid', validateProject(project))
+    res['items'] = projects
+    res['self'] = url_for('projects.get_withProjectName', project_name=project_name)
     return Standard200Response(res)
 
 @projects.route('/user/<user_ID>/project/<project_name>', methods=['GET'])
 def get_withUserAndProject(user_ID, project_name):
-    res = Service.query_ProjectWithProjectNameFromUser(user_ID, project_name)
-    if res.count == 0:
-        return responses.Standard404ErrorResponse
+    projects = []
+    res = {}
+    temp = Service.query_ProjectWithProjectNameFromUser(user_ID, project_name)
+    if temp == None:
+        return responses.Standard404ErrorResponse()
+
+    projects = generateProjectLinks(temp)
+    res['items'] = projects
+    res['self'] = url_for('projects.get_withUserAndProject',user_ID=user_ID, project_name=project_name)
     return Standard200Response(res)
 
 
@@ -131,8 +145,15 @@ def post_withUserAndProject(user_ID, project_name):
     if isinstance(project, tuple):
         return project
     else:
-        res = Service.insert_ProjectWithProjectNameFromUser(project)
+        Service.insert_ProjectWithProjectNameFromUser(project)
+        projects = []
+        res = {}
+        projects = generateProjectLinks(project)
+        res['items'] = projects
+        res['self'] = url_for('projects.get_withUserAndProject', user_ID=user_ID, project_name=project_name)
         return Standard200Response(res)
+
+
 
 
 @projects.route('/user/<user_ID>/project/<project_name>', methods=['DELETE'])
