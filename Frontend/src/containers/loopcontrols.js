cac68440270f63ee9497be9aa211b7a09de5895d
Simon Thormeyer <49559340+SimonThormeyer@users.noreply.github.com>
2020-07-01 21:48:35 +0200

    show loop controls for current track only, update key bindings (#191)
    * show loop controls for current track only
    
    * move number key handling to container
    
    now the keys pressed always correspond to the loopers shown on screen
    
    * fix stacking and animation
    
    Co-authored-by: SimonThormeyer <simonthormeyer@gmail.com>

--- Frontend/src/containers/loopcontrols.js
+++ Frontend/src/containers/loopcontrols.js
@@ -1,22 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useGlobalState } from "../GlobalState"
 import Loopicon from '../components/loopicon';
+import useEventListener from "./../UseEventListener";
 
 //wrapper component for the Loopicon component
 function Loopcontrols() {
 
     const [runningLoopers] = useGlobalState('runningLoopers');
+    const [canvasId] = useGlobalState('canvasId');
+    const [overlayIsOpen,] = useGlobalState('overlayIsOpen');
+    const [updated, forceUpdate] = useState(true);
+
+    // build an array containing only the keys of the Loopers belonging to current track / canvas
+    let displayedLoopersIDs = [];
+    Array.from(runningLoopers.keys()).forEach((id) => {
+        if(runningLoopers.get(id).canvasID === canvasId)
+        displayedLoopersIDs.push(id);
+    })
+
+    //----------- KEY-BINDINGS ------------
+    const handleKeyDown = event => {
+            if (overlayIsOpen) return;
+            // start/stop muting with Number Keys
+            let keyNumber = event.keyCode - 49;
+            if (keyNumber < 10 && keyNumber < displayedLoopersIDs.length) {
+                if (runningLoopers.get(displayedLoopersIDs[keyNumber])) {
+                    runningLoopers.get(displayedLoopersIDs[keyNumber]).toggleMute();
+                    forceUpdate(!updated); // forces children to re-render and thus update their state (show that they are muted)
+                }
+            }
+        }
+
+    useEventListener("keydown", handleKeyDown);
 
     return (
         < div className="loopcontrols" >
             {
-                Array.from(runningLoopers.keys()).map((id) => {
-                return (
-
-                    <Loopicon id={id} key={`loop_${id}`} />
-
-                )
-            })
+               displayedLoopersIDs.map((id) => {
+                    return <Loopicon id={id} key={`loop_${id}`} muted={runningLoopers.get(id).muted} />
+                })
             }
 
         </div >
