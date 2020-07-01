ca3c6f04a5c5f623fe5aa18d64fc1edfa37e622a
SimonThormeyer <simonthormeyer@gmail.com>
2020-07-01 12:52:36 +0200

    enable project sharing via URL
    by adding according routes to frontend

---
+++ Frontend/src/containers/OpenProjectsContainer.js
@@ -0,0 +1,32 @@
+import React, { useEffect } from 'react';
+import OpenProjects from '../components/OpenProjects'
+import { ReactComponent as DeleteIcon } from '../img/delete.svg';
+import { useGlobalState } from "../GlobalState";
+import { useParams, Link } from "react-router-dom";
+
+// this is the OpenProject Component just with the possibility to close it
+export default function OpenProjectContainer() {
+    const [, setOverlayIsOpen] = useGlobalState('overlayIsOpen');
+    const { user, projectName } = useParams();
+
+    // setOverlay is Open true when Component mounts, false when it unmounts
+    useEffect(() => {
+        setOverlayIsOpen(true);
+        return () => {
+            setOverlayIsOpen(false);
+        }
+    }, [setOverlayIsOpen])
+
+    return (
+        <>
+                <div className="saveOpenUnderlay"></div>
+                <div id="openOverlay">
+                    <Link as='li' to='/'>
+                        <DeleteIcon id="closeOpenOverlay" />
+                    </Link>
+                    <OpenProjects user={user} projectName={projectName} />
+                </div>
+        </>
+    )
+
+}
