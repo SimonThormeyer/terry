f7d599910d1576f3fef936d56e669b640a633d0c
Simon Thormeyer <simonthormeyer@gmail.com>
2020-07-07 15:59:11 +0200

    first slide but wrong

--- Frontend/src/containers/CanvasContainer.js
+++ Frontend/src/containers/CanvasContainer.js
@@ -1,48 +1,45 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useRef } from 'react'
 import Canvas from '../components/threeJS/canvas';
 import { useGlobalState } from "../GlobalState.js"
 import { useTransition, a } from 'react-spring'
 
 const canvases = [
-    ({ style }) => <a.div style={{...style}} ><Canvas /></a.div>,
-    ({ style }) => <a.div style={{...style}} ><Canvas /></a.div>,
-    ({ style }) => <a.div style={{...style}} ><Canvas /></a.div>
+    ({ style }) => <a.div style={{ ...style }} ><Canvas /></a.div>,
+    ({ style }) => <a.div style={{ ...style }} ><Canvas /></a.div>,
+    ({ style }) => <a.div style={{ ...style }} ><Canvas /></a.div>
 ]
 
-const items = []
 
 const CanvasContainer = () => {
 
     const [id,] = useGlobalState('canvasId');
 
-    const transitions = useTransition(items, {
-      from: { opacity: 0,},
-      enter: { opacity: 1,},
-      leave: { opacity: 0,},
-    })
+    const transitions = useTransition([canvases[id]], {
+        from: { transform: 'translate3d(100%,0,0)' },
+        enter: { transform: 'translate3d(0%,0,0)' },
+        leave: { transform: 'translate3d(-50%,0,0)' },
 
-    useEffect(() => {
-        items.pop()
-        items[0] = canvases[id];
-    },[id])
+        expires: 500,
+    })
+    
+
 
     return <div className="simple-trans-main">
-        
-          {/* {transitions.map(({ item, props, key }) => {
+
+        {/* {transitions.map(({ item, props, key }) => {
             const Cvs = canvases[item]
             return <Cvs key={key} style={props} />
             console.log(transitions)
             return <div/>
         })} */}
-            {
-                transitions((style, Item) => {
-                    // 3. Render each item
-                    console.log(style)
-                    return <a.div style={style}>{<Item style={style}></Item>}</a.div>
-                  })
-            }
-      
-        </div>
+        {
+            transitions((style, Item) => {
+                // 3. Render each item
+                return <a.div style={style}>{<Item style={style}></Item>}</a.div>
+            })
+        }
+
+    </div>
 }
 
 export default CanvasContainer;
