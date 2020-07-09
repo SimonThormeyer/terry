175c551a0c2ec8a7bac147c4128aa14273c31dd3
lunarie16 <47414934+lunarie16@users.noreply.github.com>
2020-07-09 21:32:58 +0200

    changing docker-compose files for starting front- and backend on its own (#216)

--- Backend/api/__init__.py
+++ Backend/api/__init__.py
@@ -10,37 +10,9 @@ log = app.logger
 CORS(app)
 log = app.logger
 
-#mongo_host = os.environ['MONGO_HOST']
+
 mongo = PyMongo(app, uri='mongodb://mongo:27017/Projects')
 db = mongo.db
-
-
-
-log.info('list collections')
-for x in db.list_collection_names():
-  log.info(len(x))
-  log.info(str(x))
-log.info(str(db.list_collection_names()))
-
-# client = MongoClient()
-# db2 = client.projects
-#
-# log.info(client.list_databases())
-# log.info(db2.list_collection_names())
-#db2.drop_collection('Projects')
-#proj_col = db['Projects']
-
-# client = pymongo.MongoClient("localhost:27017")
-# log.info('Client '+ str(client))
-# db = client["projects"]
-# log.info('DB '+str(db))
-# mycol = db["project"]
-# log.info("List of collections \n--------------------------")
-# for x in db.list_collection_names():
-#   log.info(len(x))
-#   log.info(x)
-
-
 ma = Marshmallow(app)
 
 
