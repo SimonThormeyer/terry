5116c61a2e0f3e30b8c7b2e995349b38aa2b7228
Simon Thormeyer <simonthormeyer@gmail.com>
2020-07-16 18:45:34 +0200

    start button toggles full screen

--- Frontend/src/store.js
+++ Frontend/src/store.js
@@ -1,17 +1,22 @@
 import create from 'zustand'
 
-const [useStore, ] = create((set, get) => {
+const [useStore,] = create((set, get) => {
 
   return {
     loadingProject: false,
+    appRef: undefined,
 
     functions: {
       dotHasLoaded() {
-        set({loadingProject: false})
+        set({ loadingProject: false })
       },
       startLoading() {
-          set({loadingProject: true})
-      }
+        set({ loadingProject: true })
+      },
+      setAppRef(appRef) {
+        set({ appRef: appRef })
+      },
+      
     }
   }
 })
